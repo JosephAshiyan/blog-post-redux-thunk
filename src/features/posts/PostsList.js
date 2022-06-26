a5c3e077c126f969bdbc2f7c7993e5be750f6040
JosephAshiyan <josephasiyan@gmail.com>
2022-06-26 19:00:56 +0300

    extended post app

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,30 +1,30 @@
 import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postsSlice";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import { selectPostIds } from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
+import { useGetPostsQuery } from './postsSlice';
 
 const PostsList = () => {
-    const posts = useSelector(selectAllPosts)
+    const {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetPostsQuery()
 
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    const orderedPostIds = useSelector(selectPostIds)
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-            <p className="postCredit">
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
-            </p>
-            <ReactionButtons post={post} />
-        </article>
-    ))
+    let content;
+    if (isLoading) {
+        content = <p>"Loading..."</p>;
+    } else if (isSuccess) {
+        content = orderedPostIds.map(postId => <PostsExcerpt key={postId} postId={postId} />)
+    } else if (isError) {
+        content = <p>{error}</p>;
+    }
 
     return (
         <section>
-            <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { extendedApiSlice } from './features/posts/postsSlice';
+import { usersApiSlice } from './features/users/usersSlice';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+store.dispatch(extendedApiSlice.endpoints.getPosts.initiate());
+store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
